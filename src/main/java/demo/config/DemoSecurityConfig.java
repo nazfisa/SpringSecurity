e1e699af83f71c11a0b6229df24ddc486dbab82e
nazfisa <nazimuddinasifiiuc@gmail.com>
2020-08-18 23:20:29 +0600

    adding jdbc

--- src/main/java/demo/config/DemoSecurityConfig.java
+++ src/main/java/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,6 @@
 package demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,16 +8,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+    // add a reference to our security data source
 
+    @Autowired
+    private DataSource securityDataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication().withUser(users.username("nazim").password("nazim").roles("EMPLOYEE"))
-                .withUser(users.username("asif").password("asif").roles("EMPLOYEE","MANAGER"))
-                .withUser(users.username("admin").password("admin").roles("EMPLOYEE","ADMIN"));
+        // use jdbc authentication ... oh yeah!!!
+
+        auth.jdbcAuthentication().dataSource(securityDataSource);
     }
 
     @Override
